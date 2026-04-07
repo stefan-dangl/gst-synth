4bc454661b3401187499e29f49f480bab3a61782
Stefan Dangl <50875629+stefan-dangl@users.noreply.github.com>
2026-04-08 00:06:23 +0200

    feat: Add base notes to gui

--- gst-synth/src/gui.rs
+++ gst-synth/src/gui.rs
@@ -1,11 +1,12 @@
+use crate::gui::key::key;
 use crate::types::{Command, Note};
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button, glib};
+use gtk::{Application, ApplicationWindow, Box as GtkBox, Orientation, glib};
 use gtk4 as gtk;
 
-pub fn draw_gui(command_tx: async_channel::Sender<Command>) -> glib::ExitCode {
-    println!("!!! draw gui");
+mod key;
 
+pub fn draw_gui(command_tx: async_channel::Sender<Command>) -> glib::ExitCode {
     let application = Application::builder()
         .application_id("com.example.FirstGtkApp")
         .build();
@@ -15,22 +16,27 @@ pub fn draw_gui(command_tx: async_channel::Sender<Command>) -> glib::ExitCode {
             .application(app)
             .title("First GTK Program")
             .default_width(350)
-            .default_height(70)
+            .default_height(200)
             .build();
 
-        let button = Button::with_label("Click me!");
-        let command_tx_clone = command_tx.clone();
-        button.connect_clicked(move |_| {
-            eprintln!("Clicked!");
-            command_tx_clone
-                .send_blocking(Command::ChangeNote(Note::A))
-                .expect("failed to send data through channel");
-        });
-        window.set_child(Some(&button));
+        let keyboard = GtkBox::new(Orientation::Horizontal, 12);
+        keyboard.set_margin_top(24);
+        keyboard.set_margin_bottom(24);
+        keyboard.set_margin_start(24);
+        keyboard.set_margin_end(24);
+
+        keyboard.append(&key(command_tx.clone(), Note::C, "C"));
+        keyboard.append(&key(command_tx.clone(), Note::D, "D"));
+        keyboard.append(&key(command_tx.clone(), Note::E, "E"));
+        keyboard.append(&key(command_tx.clone(), Note::F, "F"));
+        keyboard.append(&key(command_tx.clone(), Note::G, "G"));
+        keyboard.append(&key(command_tx.clone(), Note::A, "A"));
+        keyboard.append(&key(command_tx.clone(), Note::B, "B"));
+
+        window.set_child(Some(&keyboard));
 
         window.present();
     });
 
-    println!("!!! application run");
     application.run()
 }
